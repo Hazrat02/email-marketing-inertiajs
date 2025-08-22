94a61f55a341b45aa778d42b54521c66286febde
Hazrat02 <hazratbd80@gmail.com>
2025-08-22 13:03:22 -0700

    dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,18 +5,123 @@ import Welcome from '@/Components/Welcome.vue';
 
 <template>
     <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+        <section class="mt-5">
+            <div class="container-fluid">
+            <div class="row size-column"> 
+              <div class="col-xxl-9 box-col-12">
+                <div class="row"> 
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="card small-widget">
+                      <div class="card-body total-project border-b-primary "><span class="text-danger">Total Project</span>
+                        <div class="project-details"> 
+                          <div class="project-counter"> 
+                            <h2 style="font-weight: 600;">1,523</h2><span class="text-danger">(This month)</span>
+                          </div>
+                          <div class="product-sub bg-primary-light">
+                            <svg class="invoice-icon">
+                              <use href="https://admin.pixelstrap.net/riho/assets/svg/icon-sprite.svg#color-swatch"></use>
+                            </svg>
+                          </div>
+                        </div>
+                        <ul class="bubbles">
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="card o-hidden small-widget">
+                      <div class="card-body total-Progress border-b-warning border-2"> <span class="f-light f-w-500 f-14" >In Progress</span>
+                        <div class="project-details">
+                          <div class="project-counter">
+                            <h2 class="f-w-600">836</h2><span class="f-12 f-w-400">(This month) </span>
+                          </div>
+                          <div class="product-sub bg-warning-light"> 
+                            <svg class="invoice-icon">
+                              <use href="https://admin.pixelstrap.net/riho/assets/svg/icon-sprite.svg#tick-circle"></use>
+                            </svg>
+                          </div>
+                        </div>
+                        <ul class="bubbles">
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="card o-hidden small-widget">
+                      <div class="card-body total-Complete border-b-secondary border-2"><span class="f-light f-w-500 f-14">Complete</span>
+                        <div class="project-details">
+                          <div class="project-counter">
+                            <h2 class="f-w-600">475</h2><span class="f-12 f-w-400">(This month) </span>
+                          </div>
+                          <div class="product-sub bg-secondary-light"> 
+                            <svg class="invoice-icon">
+                              <use href="https://admin.pixelstrap.net/riho/assets/svg/icon-sprite.svg#add-square"></use>
+                            </svg>
+                          </div>
+                        </div>
+                        <ul class="bubbles"> 
+                          <li class="bubble"> </li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"> </li>
+                          <li class="bubble"></li>
+                          <li class="bubble"> </li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"> </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="card o-hidden small-widget">
+                      <div class="card-body total-upcoming"><span class="f-light f-w-500 f-14">Upcoming</span>
+                        <div class="project-details"> 
+                          <div class="project-counter">
+                            <h2 class="f-w-600">189</h2><span class="f-12 f-w-400">(This month) </span>
+                          </div>
+                          <div class="product-sub bg-light-light"> 
+                            <svg class="invoice-icon">
+                              <use href="https://admin.pixelstrap.net/riho/assets/svg/icon-sprite.svg#edit-2"></use>
+                            </svg>
+                          </div>
+                        </div>
+                        <ul class="bubbles"> 
+                          <li class="bubble"> </li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                          <li class="bubble"></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
+            </div>
+        </section>
     </AppLayout>
 </template>
